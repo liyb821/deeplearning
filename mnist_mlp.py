976702d2b18f2c2e2cea61b74b1eb2616b3bd362
liyb821 <liyb821@gmail.com>
2017-05-14 20:38:57 +0800

    modify the comment

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -29,7 +29,7 @@ hidden2 = tf.nn.relu(tf.matmul(hidden1_drop, w2) + b2)
 hidden2_drop = tf.nn.dropout(hidden2, keep_prob)
 y_ = tf.nn.softmax(tf.matmul(hidden2_drop, w3) + b3)
 
-# step2: loss and optimizer
+# step2: define loss and optimizer
 # loss function
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y*tf.log(tf.clip_by_value(y_,1e-10,1.0)), reduction_indices=[1]))
 all_weights = tf.trainable_variables()
